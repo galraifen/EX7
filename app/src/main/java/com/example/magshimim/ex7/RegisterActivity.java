c7857b6b0345cc20d7a4377573e01d38bd1a88da
galraifen <galraifen15@gmail.com>
2017-11-24 11:26:03 +0200

    EX7

--- app/src/main/java/com/example/magshimim/ex7/RegisterActivity.java
+++ app/src/main/java/com/example/magshimim/ex7/RegisterActivity.java
@@ -1,13 +1,37 @@
 package com.example.magshimim.ex7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class RegisterActivity extends Activity {
-
+    public final int REGISTER_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    public void sendBack(View view)
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+        EditText fName = (EditText)findViewById(R.id.efName);
+        EditText lName = (EditText)findViewById(R.id.elName);
+        String lastName = lName.getText().toString();
+        String firstName = fName.getText().toString();
+        RadioGroup radioGroup = (RadioGroup)findViewById(R.id.rGroup);
+        int genderId = radioGroup.getCheckedRadioButtonId();
+        RadioButton genderB = (RadioButton)findViewById(genderId);
+        String gender = genderB.getText().toString();
+        intent.putExtra("lName",lastName);
+        intent.putExtra("fName",firstName);
+        intent.putExtra("gender",gender);
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+
 }
